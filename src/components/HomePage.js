fb5b7f2b669af43dbef1fe06f28c825f6d62e507
zuhayrhasan <zuhxyr@gmail.com>
2022-12-03 10:38:46 -0500

    Home Page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,20 @@ export const HomePage = () => {
     return (
         <div>
             <Navbar />
-            <h1>Home</h1>
+            <br></br>
+            <br></br>
+            <div align="center">
+                <h2>Welcome back, PLACEHOLDER!</h2>
+                <h4>What would you like to accomplish?</h4>
+                <br></br>
+            </div>
+            <div class="row" align="center">
+                <div>
+                    <img src={require("./../images/view_classes.png")} alt="View Classes" width="400px" style={{padding: '50px'}}></img>
+                    <img src={require("./../images/browse_notes.png")} alt="View Classes" width="400px" style={{padding: '50px'}}></img>
+                    <img src={require("./../images/ask_question.png")} alt="View Classes" width="400px" style={{padding: '50px'}}></img>
+                </div>
+            </div>
         </div>
     );
 }
