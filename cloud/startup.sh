b06beee268588eec66b31d06b36b051501e22576
Rohan <rkamraly@uwo.ca>
2022-12-03 18:09:10 -0500

    Deletion to rename Cloud folder

--- cloud/startup.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-# crontab config below
-# 0 9 * * * root reboot
-# @reboot /home/ubuntu/startup.sh
-
-cd /var/www/StudyNote && git pull
-screen -S studynote -dm bash -c 'cd /var/www/StudyNote/Client; npm start'
