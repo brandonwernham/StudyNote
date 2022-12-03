3e2c19e52c31bfb7b6f4b8eff6ef0506535df6f4
emachej <emachej@uwo.ca>
2022-12-03 16:15:19 -0500

    added some test files (commented out)

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./UploadPage.css";
 import Navbar from './Navbar';
+import database from '../database/dbConfig';
+
 
 export const UploadPage = () => {
     return (
@@ -11,8 +13,20 @@ export const UploadPage = () => {
             <div>
                <h1 className='upload-title'>Upload A File</h1> 
             </div>
+            <div>
+                <button onClick = {addTestUser}>Add Test User</button>
+            </div>
         </div>
     );
 }
+
+//test button
+function addTestUser(){
+    //database.query("INSERT INTO UserInfo (User_ID, Username, UserPassword) VALUES (2, FTM, FTMPassword)");
+    console.log("user added");
+}
+
+
+
 
 export default UploadPage;
---
+++ src/database/dbConfig.js
@@ -0,0 +1,19 @@
+// const mysql = require("mysql");
+
+
+// // Connects to the db using given values
+// const database = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "bonyRoot",
+//   database: "StudyNote",
+//   //multipleStatements: true,
+// });
+
+// // Shows error, otherwise state that the connection was successful
+// database.connect((error) => {
+//   if (error) throw error;
+//   console.log("Database connection successful.");
+// });
+
+// module.exports = database;
