74a60cd0a02c2ca4096223ff8abd78f303549d8f
Rohan <rkamraly@uwo.ca>
2022-12-03 17:15:54 -0500

    fixed startup script + server listens at react app

--- cloud/startup.sh
+++ cloud/startup.sh
@@ -1,10 +1,8 @@
 #/bin/bash
 
 # crontab config below
-# 0 9 * * * sudo /sbin/shutdown -r
-# @reboot sleep 60 && sudo /home/ubuntu/startup.sh
+# 0 9 * * * root reboot
+# @reboot /home/ubuntu/startup.sh
 
 cd /var/www/StudyNote && git pull
-rm -r /var/www/StudyNote/build
-cd /var/www/StudyNote && npm run build 
-sudo systemctl restart nginx
+/usr/bin/screen -S studynote -dm cd /var/www/StudyNote && npm run start
