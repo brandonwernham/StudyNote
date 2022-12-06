5f7dfe18a4714a37b198b3be866b7dee70e72e78
Rohan <rkamraly@uwo.ca>
2022-12-06 17:33:17 -0500

    cors options

--- Server/index.js
+++ Server/index.js
@@ -11,14 +11,20 @@ const database = mysql.createPool({
     database: "StudyNoteDB",
 });
 
-app.use(cors());
+var corsOption = {
+    origin: '*',
+    methods: ["GET", "POST"],
+    credentials: true
+}
 
-app.use((req, res, next) => {
+app.use(cors(corsOption));
+
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', `*`) ;
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Methods', '*');
     next();
-});
+});*/
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
