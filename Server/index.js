374d3fddfebfcd1d796f2ee5041b2f75349e5e66
Rohan <rkamraly@uwo.ca>
2022-12-06 17:54:04 -0500

    more cors fix

--- Server/index.js
+++ Server/index.js
@@ -19,12 +19,12 @@ var corsOption = {
 
 app.use(cors(corsOption));
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', `*`) ;
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token');
     res.header('Access-Control-Allow-Methods', '*');
     next();
-});*/
+});
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
