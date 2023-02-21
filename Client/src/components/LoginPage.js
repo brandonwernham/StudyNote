ccfe93c044e46319e0b494dfdc6b926bb1178562
brandonwernham <brandonwern01@gmail.com>
2023-02-21 08:26:14 -0500

    Login page email must be valid

--- Client/src/components/LoginPage.js
+++ Client/src/components/LoginPage.js
@@ -9,9 +9,15 @@ export const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loginStatus, setLoginStatus] = useState("");
+    const [invalidEmailError, setInvalidEmailError] = useState("");
 
     //posts entered info from signup form to server and database
     const submitLogin = () => {
+        // Checks if email is valid
+        if (!isValidEmail(email)) {
+            setInvalidEmailError("Not a valid email");
+            return;
+        }
         Axios.post("https://studynote.ca/api/login", {
             email: email,
             password: password,
@@ -25,6 +31,10 @@ export const LoginPage = () => {
             }
         })
     };
+
+    const isValidEmail = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };    
     
     const redirRegister = () => {
         navigate("/register")
@@ -38,12 +48,14 @@ export const LoginPage = () => {
             <br></br>
             <div className='login-form'>
                 <form>
-                    <input type="text" placeholder="Email" className='username' id='username' onChange={(e) => {setEmail(e.target.value)}}></input>
+                    <input type="text" placeholder="Email" className='username' id='username' onChange={(e) => {setEmail(e.target.value); setInvalidEmailError("")}}></input>
                     <br></br>
                     <input type="password" placeholder="Password" className='password' id='password' onChange={(e) => {setPassword(e.target.value)}}></input>
                 </form>
             </div>
             <h5 align="center">{loginStatus}</h5>
+            <h5 align="center" style={{color: "red"}}>{invalidEmailError}</h5>
+
             <div className='form-button' align='center'>
                 <button type='submit' name='login' className='login-button' onClick={submitLogin}>Log In</button>
                 <button name='register' className='login-button' onClick={redirRegister}>Register</button>
