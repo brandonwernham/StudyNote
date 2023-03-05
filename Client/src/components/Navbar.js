50a305353eb6a79b9c1f5b19c9a7f188793659bf
brandonwernham <brandonwern01@gmail.com>
2023-03-05 16:51:26 -0500

    Background and nav changes

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -1,19 +1,32 @@
+import { useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <a href="/" className="title">StudyNote</a>
-            <div className="pages">
-                <a class="active" href="/search">Search</a>
-                <a href="/classes">Classes</a>
-                <a href="/groups">Groups</a>
-                <a href="/upload">Upload</a>
-            </div>
-            
-            <div class="container">
-                <a href="/login">Login</a>
-            </div>
-        </nav>
-    );
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="navbar">
+        <a href="/" className="title">
+            StudyNote
+        </a>
+        <div className="pages">
+            <a href="/search" className={pathname === "/search" ? "active" : ""}>
+            Search
+            </a>
+            <a href="/classes" className={pathname === "/classes" ? "active" : ""}>
+            Classes
+            </a>
+            <a href="/groups" className={pathname === "/groups" ? "active" : ""}>
+            Groups
+            </a>
+            <a href="/upload" className={pathname === "/upload" ? "active" : ""}>
+            Upload
+            </a>
+        </div>
+
+        <div className="container">
+            <a href="/login">Login</a>
+        </div>
+    </nav>
+  );
 }
