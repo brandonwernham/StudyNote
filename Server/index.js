b214041e36cc6ab3705c4f995007d4487b8ee631
emachej <emachej@uwo.ca>
2023-03-13 21:14:59 -0400

    testing

--- Server/index.js
+++ Server/index.js
@@ -139,7 +139,7 @@ app.post("/api/upload", upload.fields([
     const tags = req.body.tags;
 
     
-    const sqlInsert = "INSERT INTO notes (note_name, file_path, creator_id) VALUES (?, ?, ?, ?)" //note ID is created inside the database and auto incremented - in thise case its the "insertId"
+    const sqlInsert = "INSERT INTO notes (note_name, file_path, creator_id) VALUES (?, ?, ?)" //note ID is created inside the database and auto incremented - in thise case its the "insertId"
     database.query(sqlInsert, [note_name, file_path, creator_id], (err, result) => {
         if (err) {
             res.send({err: err}) //this will be returned when duplicate entry in database, among with other errrs.
