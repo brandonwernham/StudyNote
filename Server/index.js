84a3cb0d48aa79a97dcba397891a1c9e439d23b8
doccmod <rkamraly@uwo.ca>
2023-03-16 20:03:25 -0400

    semi-reverted, needs more testing

--- Server/index.js
+++ Server/index.js
@@ -169,37 +169,28 @@ app.post("/api/upload", upload.single("note"), (req, res) => {
 
             function sqlTagsQueryFunc() {
                 return new Promise(resolve => {
-                    function sqlTagsSelectFunc() {
-                        return new Promise(resolve => {
-                            const sqlTagsQuery = "SELECT * FROM tags WHERE tag_name = ?"
-                            resolve();
-                        }); 
-                    }
-                    async function sqlTagsQueryInsertFunc() {
-                        await sqlTagsSelectFunc();
-                        database.query(sqlTagsQuery, [tag], (err, result) => {
-                            if (err) {
-                                message = message + " " + err;
-                            } else {
-                                if (result.length == 0){
-                                    database.query("INSERT INTO tags (tag_name) VALUES (?)", [tag], (err, result) =>{
-                                        if (err) {
-                                            message = message + " " + err;
-                                        }
-                                        tagID = result.insertId;
-                                        resolve();
-                                    })
-                                    
-                                }
-                                else{
-                                    tagID = result[0].tag_id;
+                    const sqlTagsQuery = "SELECT * FROM tags WHERE tag_name = ?"
+                    database.query(sqlTagsQuery, [tag], (err, result) => {
+                        if (err) {
+                            message = message + " " + err;
+                        } else {
+                            if (result.length == 0){
+                                database.query("INSERT INTO tags (tag_name) VALUES (?)", [tag], (err, result) =>{
+                                    if (err) {
+                                        message = message + " " + err;
+                                    }
+                                    tagID = result.insertId;
                                     resolve();
-                                }
+                                })
+                                
                             }
-                        })
-                        console.log(noteID)
-                    } 
-                    sqlTagsQueryInsertFunc();
+                            else {
+                                tagID = result[0].tag_id;
+                                resolve();
+                            }
+                        }
+                    })
+                    console.log(noteID)
                 });
             }
 
