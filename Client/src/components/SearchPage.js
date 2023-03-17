98118154e9a0ab5fe67c96c08b70f849acb56862
zuhayrhasan <zuhxyr@gmail.com>
2023-03-17 18:24:48 -0400

    make prettier search results
    missing pdf/docx/png preview

--- Client/src/components/SearchPage.js
+++ Client/src/components/SearchPage.js
@@ -75,16 +75,20 @@ export const SearchPage = () => {
             Search
           </button>
         </form>
+        <br></br>
         {noteList.length > 0 ? (
+        <div className="note-list-wrapper">
         <div className="note-list">
-        {noteList.map((note, index) => (
-          <div key={index} className="note-box" onClick={() => { /* do something */ }}>
-            <div>File path: {note.file_path}</div>
-            <div>Tags: {note.tags}</div>
-            <div>Course code: {note.course_code}</div>
-            <div>Subject code: {note.subject_code}</div>
-          </div>
-        ))}
+          {noteList.map((note, index) => (
+            <div key={index} className="note-box" onClick={() => { /* do something */ }}>
+              <div className="note-box-bottom">
+                <div className="note-name">THIS SHOULD BE A GOOD LENGTH FOR A NAME WHAT IF IT OVERFLOWS</div>
+                <div className="note-code">{note.subject_code} {note.course_code}</div>
+                <div className="note-tags">{note.tags}</div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
         ) : (
         <div>No matching notes found.</div>
