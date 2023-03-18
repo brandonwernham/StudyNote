5fa7f6ea105dc1e81676eb7e8d436ed34c8bfdb6
brandonwernham <brandonwern01@gmail.com>
2023-03-18 11:04:58 -0400

    Made user context for login info

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,16 +6,19 @@ import GroupsPage from './components/GroupsPage';
 import UploadPage from './components/UploadPage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import { UserProvider } from "./components/UserContext";
 
 function App() {
     return (
-        <Routes>
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='search' element={<SearchPage />}></Route>
-            <Route path='classes' element={<ClassesPage />}></Route>
-            <Route path='groups' element={<GroupsPage />}></Route>
-            <Route path='upload' element={<UploadPage />}></Route>
-        </Routes>
+        <UserProvider>
+            <Routes>
+                <Route path='/' element={<HomePage />}></Route>
+                <Route path='search' element={<SearchPage />}></Route>
+                <Route path='classes' element={<ClassesPage />}></Route>
+                <Route path='groups' element={<GroupsPage />}></Route>
+                <Route path='upload' element={<UploadPage />}></Route>
+            </Routes>
+        </UserProvider>
     );
 }
 
